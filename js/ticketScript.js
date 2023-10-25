6fd990deabbbf2d2b8c44240d07289b0d039f239
IMMD3v <116383008+IMMD3v@users.noreply.github.com>
2023-10-24 22:08:17 -0300

    se añade lógica para mostrar modal con los datos que se ingresan al comprar tickets

--- js/ticketScript.js
+++ js/ticketScript.js
@@ -5,17 +5,21 @@ document.addEventListener("DOMContentLoaded", function() {
     //procede a ejecutar la función
     calcularPrecio();
 })
+
+//BLOQUE DE VARIABLES------------------------------------
 //defino las variables que voy a usar, capturo los valores de un elemento
 //mediante función getElementById por su Id único, y obtengo el value
 let ticketCategory = document.getElementById("buyFormCategory").value;
 let quantity = document.getElementById("buyFormQty").value;
 let result = document.getElementById("finalPayment");
+let modalResult = document.getElementById("modalFinalPayment");
 
 //este eventListener permite capturar permanentemente cualquier cambio en el
 //input al que se llama y lo envía a la función
 buyFormCategory.addEventListener("input", calcularPrecio);
 buyFormQty.addEventListener("input", calcularPrecio);
 
+//BLOQUE DE FUNCIONES------------------------------------
 function calcularPrecio() {
 //defino variables en el scope local
     let ticketCategory = parseFloat(buyFormCategory.value);
@@ -29,20 +33,51 @@ function calcularPrecio() {
             //el resultado se lo paso a la variable result,
             //mediante textContent que me permite definir su contenido
             result.textContent = "Total a pagar: $" + totalPayment;
+            modalResult.textContent = "Total a pagar: $" + totalPayment;
             break;
         case 2:
             totalPayment = quantity * (basePrice - (basePrice * 0.50));
             result.textContent = "Total a pagar: $" + totalPayment;
+            modalResult.textContent = "Total a pagar: $" + totalPayment;
             break;
         case 3:
             totalPayment = quantity * (basePrice - (basePrice * 0.15));
             result.textContent = "Total a pagar: $" + totalPayment;
+            modalResult.textContent = "Total a pagar: $" + totalPayment;
             break;
         case 4:
             totalPayment = quantity * basePrice;
             result.textContent = "Total a pagar: $" + totalPayment;
+            modalResult.textContent = "Total a pagar: $" + totalPayment;
             break;
         default:
             result.textContent = "Por favor elija una categoría";
+            modalResult.textContent = "Por favor elija una categoría";
     }
+}
+//función para resetear el formulario
+function resetForm() {
+    document.getElementById("buyTicketForm").reset();
+}
+//función que simularía haber comprado las entradas
+function confirmPayment() {
+    alert("Muchas gracias por tu compra!");
+}
+
+function openConfirmModal() {
+    let buyName = document.getElementById("buyFormName").value;
+    let modalName = document.getElementById("buyName");
+    modalName.textContent = buyName;
+
+    let buySurname = document.getElementById("buyFormSurname").value;
+    let modalSurname = document.getElementById("buySurname");
+    modalSurname.textContent = buySurname;
+
+    let buyEmail = document.getElementById("buyFormEmail").value;
+    let modalEmail = document.getElementById("buyEmail");
+    modalEmail.textContent = buyEmail;
+
+    let totalTickets = document.getElementById("buyFormQty").value;
+    let modalTotalTickets = document.getElementById("ticketQty");
+    modalTotalTickets.textContent = totalTickets;
 }
